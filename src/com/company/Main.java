73f97597c15627c8697d50bbcca4ab7a93a21b36
JIexa <msleha100@gmail.com>
2021-05-11 10:38:01 +0300

    Добавил извлечение операции из выражения

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
+import java.util.Scanner;
+
+import static com.company.Metods.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        System.out.println(operation(input));
+        switch (operation(input)){
+            case "+":
+
+                break;
+            case "-":
+
+                break;
+            case "*":
+
+                break;
+            case "/":
+
+                break;
+            default:
+                System.out.println("Ошибка!");
+        }
+
+        int firstNum = 1;
+        int secondNum = 2;
+        findNums(operation(input),firstNum,secondNum);
+        System.out.println(firstNum + " " + secondNum);
     }
 }
